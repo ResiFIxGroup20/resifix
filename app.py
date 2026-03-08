01c8e99854c2bda8c336e36af52e1a26a2f0fa54
ResiFlxGroup20 <ntuthukoops12@gmail.com>
2026-03-08 19:33:30 +0200

    Updated app.py with db init, confirmed gitignore

--- app.py
+++ app.py
@@ -1,1 +1,20 @@
+from flask import Flask
+from routes.auth import auth
+from routes.resident import resident
+from routes.admin import admin
+from routes.technician import technician
+from database.db import init_db
 
+app = Flask(__name__)
+app.secret_key = 'resifix_secret_key'
+
+app.register_blueprint(auth)
+app.register_blueprint(resident)
+app.register_blueprint(admin)
+app.register_blueprint(technician)
+
+with app.app_context():
+    init_db()
+
+if __name__ == '__main__':
+    app.run(debug=True)
